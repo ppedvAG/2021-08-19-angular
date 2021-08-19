3fa91ae79e4c636389cd20d2eb7bb0d61730532e
Vadzim Naumchyk <vadzimn@ppedv.de>
2021-08-19 13:53:39 +0200

    m004 demo

---
+++ TRAINER/ng12-app/src/app/demos/d04-component/d04-component.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-d04-component',
+  templateUrl: './d04-component.component.html',
+  styleUrls: ['./d04-component.component.css']
+})
+export class D04ComponentComponent
+{
+  public classProp = 'classPropValue';
+  // ohne modifier sind Props public
+  classProp1 = 'classPropValue';
+  classProp2 = 7;
+
+  classMethod1() {
+    return 'return from classMethod1';
+  }
+
+  giveClass() {
+    if(Math.random() > 0.5) {
+      return 'blueBackground';
+    } else {
+      return '';
+    }
+  }
+
+
+}
