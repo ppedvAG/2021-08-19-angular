c61559d19288a7b895c7a84f767c0fc6936c3c9b
Vadzim Naumchyk <vadzimn@ppedv.de>
2021-08-23 17:54:18 +0200

    pwa

--- TRAINER/ng12-app/src/app/app-routing.module.ts
+++ TRAINER/ng12-app/src/app/app-routing.module.ts
@@ -3,15 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { D04ComponentComponent } from './demos-mdl/d04-component/d04-component.component';
 import { D05LifecycleComponent } from './demos-mdl/d05-lifecycle/d05-lifecycle.component';
 import { OverviewDemosComponent } from './demos-mdl/overview-demos/overview-demos.component';
+import { RootComponent } from './root-mdl/root/root.component';
 import { OverviewTodosComponent } from './todos-mdl/overview-todos/overview-todos.component';
 
 const routes: Routes = [
-  { path: 'todos', component: OverviewTodosComponent },
-  { path: 'demos', component: OverviewDemosComponent, children: [
+  // { path: 'home', component: RootComponent },
+  // { path: 'todos', component: OverviewTodosComponent },
+  { path: 'todos', component: OverviewTodosComponent, loadChildren: './todos-mdl/todos-module#TodosModule' },
+/*   { path: 'demos', component: OverviewDemosComponent, children: [
     { path: 'component', component: D04ComponentComponent},
     { path: 'lifecycle', component: D05LifecycleComponent, data: {prop1: 'test',
   prop2: 'test2'}},
-  ] }
+  ] } */
+  { path: 'demos', component: OverviewDemosComponent, loadChildren: './demos-mdl/demos-module#DemosModule' }
 ];
 
 @NgModule({
--- TRAINER/ng12-app/src/app/root-mdl/app.module.ts
+++ TRAINER/ng12-app/src/app/root-mdl/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from '../app-routing.module';
-import { DemosModule} from '../demos-mdl/demos.module';
-import { TodosModule } from '../todos-mdl/todos.module';
+// import { DemosModule} from '../demos-mdl/demos.module';
+// import { TodosModule } from '../todos-mdl/todos.module';
 import { AppComponent } from './appcomponent/app.component';
 import { RootComponent } from './root/root.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,8 +19,14 @@ import { RootComponent } from './root/root.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    DemosModule,
-    TodosModule
+    // DemosModule,
+    // TodosModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [RootComponent]
